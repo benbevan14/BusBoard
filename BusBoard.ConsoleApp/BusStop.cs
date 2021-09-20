4f784aefbd17ef54c318ea9f99c340ed9baa717a
Benjamin Bevan <bbevan196@gmail.com>
2021-09-20 15:52:20 +0100

    some things work now

---
+++ BusBoard.ConsoleApp/BusStop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBoard.ConsoleApp
+{
+    public class BusStop
+    {
+        public string naptanId { get; set; }
+        public string commonName { get; set; }
+        public double distance { get; set; }
+
+        public override string ToString()
+        {
+            return naptanId + " : " + commonName + " : " + distance.ToString("0.00");
+        }
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,35 +1,75 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 using RestSharp;
 
 namespace BusBoard.ConsoleApp
 {
-    class Program
+    public class Program
     {
-        string cheltenham = "490005082S";
-        string cheltenham2 = "90001419";
-        string corndel = "490008660N";
+        //string cheltenham = "490005082S";
+        // 910GCHLTNHM
+        // 1600GL3959
+        // E0011703
+
         static void Main(string[] args)
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            Console.WriteLine("Hello world");
+            Console.WriteLine("start the ting");
 
-            var client = new RestClient("https://api.tfl.gov.uk/StopPoint");
+            var client = new RestClient("https://api.tfl.gov.uk");
 
-            var request = new RestRequest("490005082S/Arrivals", DataFormat.Json);
+            Location hop = new Location(51.49984, -0.124663, "SW1A0AA", 500);
 
-            var response = client.Get(request);
+            var request = new RestRequest("StopPoint/?lat=" + hop.Latitude.ToString() 
+                                        + "&lon=" + hop.Longitude.ToString()
+                                        + "&stopTypes=NaptanPublicBusCoachTram" + "&radius=" + hop.Radius.ToString(), Method.GET);
 
-            foreach (var s in response.Content.Split('{', '}'))
-            { 
-                foreach (var entry in s.Split(','))
-                {
-                    Console.WriteLine(entry);
-                }
-                
+            request.AddHeader("app_id", "f1b7f9a2b331460dad132b97645ca624");
+            request.AddHeader("app_key", "b47323ef3b2145e78eda7cf2321e28f9");
+
+            StopPoint response = client.Execute<StopPoint>(request).Data;
+
+            foreach (BusStop b in response.stopPoints)
+            {
+                Console.WriteLine(b.ToString());
             }
-            //Console.WriteLine(response.Content.Spl);
+
+            //foreach (BusStop b in response)
+            //{
+            //    Console.WriteLine(b.ToString());
+            //}
+
+            //dynamic content = JsonConvert.DeserializeObject(response.Content);
+
+            //var stops = content["stopPoints"];
+
+            //List<BusStop> busStops = new List<BusStop>();
+
+            //foreach (var entry in stops)
+            //{
+            //    string data = entry.ToString().Replace(@"""", "");
+            //    string naptanId = Regex.Match(data, @"naptanId: [^\n,]+").Value.Substring(10);
+            //    string common = Regex.Match(data, @"commonName: [^\n,.]+").Value.Substring(12);
+            //    string distance = Regex.Match(data, @"distance: [^\n,]+").Value.Substring(10);
+            //    busStops.Add(new BusStop(naptanId, common, double.Parse(distance)));
+            //}
+
+            //foreach (var bus in busStops)
+            //{
+            //    Console.WriteLine(bus.ToString());
+            //}
+
+            //Console.WriteLine("\n\n\n\n\n");
+
+            //request = new RestRequest("StopPoint/" + busStops[2].naptanId + "/Arrivals");
+
+            //response = client.Get(request);
+
+            //Console.WriteLine(response.Content);
 
             Console.WriteLine("done");
 
