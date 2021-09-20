5c2565b06f723fdd6e7e75cacf1618c5302ff867
Benjamin Bevan <bbevan196@gmail.com>
2021-09-20 11:46:08 +0100

    initial commit

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using RestSharp;
 
 namespace BusBoard.ConsoleApp
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
+        string cheltenham = "490005082S";
+        string cheltenham2 = "90001419";
+        string corndel = "490008660N";
+        static void Main(string[] args)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            Console.WriteLine("Hello world");
+
+            var client = new RestClient("https://api.tfl.gov.uk/StopPoint");
+
+            var request = new RestRequest("490005082S/Arrivals", DataFormat.Json);
+
+            var response = client.Get(request);
+
+            foreach (var s in response.Content.Split('{', '}'))
+            { 
+                foreach (var entry in s.Split(','))
+                {
+                    Console.WriteLine(entry);
+                }
+                
+            }
+            //Console.WriteLine(response.Content.Spl);
+
+            Console.WriteLine("done");
+
+            Console.ReadLine();
+        }
     }
-  }
 }
